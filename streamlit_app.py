05ea36f6e655b9fb2720cf3a6bceadaabc29ef0a
Lukas-MPD <115532812+Lukas-MPD@users.noreply.github.com>
2024-07-10 13:24:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,7 +217,7 @@ with col1:
         legend_name='Legend Name'
     ).add_to(m)
 
-    st_folium(m, width=700, height=500)
+    st_folium(m)
     
     #first_year = gdp_df[gdp_df['Year'] == from_year]
     #last_year = gdp_df[gdp_df['Year'] == to_year]
