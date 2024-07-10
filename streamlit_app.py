60467223578d9c55ed0f0471a0f462e8324a929e
Lukas-MPD <115532812+Lukas-MPD@users.noreply.github.com>
2024-07-10 16:57:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,7 @@ with col1:
     
     folium_static(m)
 
-    #m.save('map.html')
+    m.save('map.html')
 
     # Display the map in Streamlit using an iframe
     #components.iframe(src="map.html", width=700, height=500)
