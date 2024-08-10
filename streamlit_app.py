a015a9d1f2ab750fc7bcd224850cb24ef8131db7
Lukas-MPD <115532812+Lukas-MPD@users.noreply.github.com>
2024-08-10 18:55:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -440,6 +440,8 @@ with mainpage:
         # Group by month and calculate the mean of the 'value' column
         monthly_mean = monthly_mean.groupby(['geo', 'month'])['value'].mean().reset_index()
 
+        monthly_mean = monthly_mean.sort_values(by='value', ascending=True)
+        
         st.write(monthly_mean)
 
         fig_bar_polar = px.bar_polar(monthly_mean,
