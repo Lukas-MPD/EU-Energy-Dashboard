0970c1562fce188f0b25dd62814984cf0e7d6bfc
Lukas-MPD <115532812+Lukas-MPD@users.noreply.github.com>
2024-08-10 19:17:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -439,8 +439,6 @@ with mainpage:
         
         # Group by month and calculate the mean of the 'value' column
         monthly_mean = monthly_mean.groupby(['geo', 'month'])['value'].mean().reset_index()
-
-        monthly_mean = monthly_mean.sort_values(by='value', ascending=True)
         
         st.write(monthly_mean)
 
