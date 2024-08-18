f31b3255a3c2292cd5ef7da3801f68a52ead2af2
Lukas-MPD <115532812+Lukas-MPD@users.noreply.github.com>
2024-08-18 17:35:57 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -326,7 +326,7 @@ with sidebar:
         dict_filters.update({i: selec})
 
         if dic_df[i]['descr'] is not None:
-            with st.expander(icon='i'):
+            with st.expander("",icon='i'):
                 dic_df[i]['descr']
 
 
